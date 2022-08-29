d0aae8a33b89bea73eaa913ba6c6b10c7168e525
elnurxo <elnurykh@code.edu.az>
2022-08-30 01:17:34 +0400

    custom array methods done!

---
+++ app.js
@@ -0,0 +1,121 @@
+var arr = [1,3,5];
+console.log('my array: ',arr);
+
+//CUSTOM MAP METHOD +
+Array.prototype.customMap = function (cb) {
+    // `this` keyword points to the array itself
+    const result = [];
+ 
+    for (let i = 0; i < this.length; i++) {
+        result.push(cb(this[i], i, this));
+    }
+    return result;
+ }
+
+//CUSTOM SOME METHOD +
+Array.prototype.customSome = function(cb){
+
+   for (let i = 0; i < this.length; i++) {
+        if (cb(this[i])) {
+            return true;
+        }
+   }
+   return false;
+}
+
+//CUSTOM EVERY METHOD +
+Array.prototype.customEvery = function(cb){
+
+    for (let i = 0; i < this.length; i++) {
+         if (!cb(this[i])) {
+             return false;
+         }
+    }
+    return true;
+ }
+//CUSTOM REDUCE METHOD +
+Array.prototype.customReduce = function(cb){
+    var sum = 0;
+
+    for (const item of this) {
+        cb(sum+=item);
+    }
+    return sum;
+}
+
+//CUSTOM forEACH METHOD +
+Array.prototype.customForEach = function (cb) {
+    for (let i = 0; i < this.length; i++) {
+        cb(this[i], i, this);
+    }
+    return this;
+ }
+
+//CUSTOM FLAT METHOD
+Array.prototype.customFlat = function(depth){
+    if (depth === undefined) {
+        depth = 1;
+      }
+      result = [].concat(...this);
+
+    for (let i = 0; i < result.length; i++) {
+       if (Array.isArray(result[i])) {
+        if (depth>0) {
+            result.push(...result[i]);
+            result.splice(i,1);
+            depth--;
+         }
+       }
+    }
+    return result;
+}
+
+//CUSTOM REVERSE METHOD +
+Array.prototype.customReverse = function () {
+
+    var result = [];
+
+    for (let i = this.length-1; i >= 0; i--) {
+        result.push(this[i]);
+    }
+    for (let i = 0; i < this.length; i++) {
+       this[i] = result[i];
+    }
+    
+    return this;
+ }
+
+//CUSTOM JOIN METHOD +
+Array.prototype.customJoin = function (str) {
+    var strResult = "";
+    if (str===undefined) {
+        str = ',';
+    }
+    for (let i = 0; i < this.length; i++) {
+        strResult += this[i];
+        if (i<this.length-1) {
+            strResult += str;
+        }
+    }
+    return strResult;
+}
+
+//CUSTOM INCLUDES METHOD +
+Array.prototype.customIncludes = function(searchElement,fromIndex){
+    if (fromIndex===undefined) {
+        for (let i = 0; i < this.length; i++) {
+            if (this[i]==searchElement) {
+             return true;
+            }
+         }
+         return false;
+    }
+    else{
+        for (let i = fromIndex; i < this.length; i++) {
+            if (this[i]==searchElement) {
+             return true;
+            }
+         }
+         return false;
+    }
+}
